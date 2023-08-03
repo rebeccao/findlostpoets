f230ed9bc3d7e895721029813abcfa48fd612eb5
rebeccao <rebecca@stylsavvy.com>
2023-08-03 13:57:04 -0600

    Added Tailwindcss.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,7 +10,9 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Remix
+      </h1>
       <ul>
         <li>
           <a
