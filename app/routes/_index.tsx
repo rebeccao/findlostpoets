6a89cfcf2703b13ac16daeb929122812fcb0a164
rebeccao <rebecca@stylsavvy.com>
2023-08-03 16:53:40 -0600

    Modified index.tsx.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { json } from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -8,7 +10,32 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+type Book = {
+  title: string;
+  genre: string;
+}
+type Books = Array<Book>;
+type LoaderData = {
+  books: Books;
+}
+
+export const loader = async () => {
+  return json<LoaderData>({
+    books: [
+      {
+        title: 'Harry Potter and the Deathly Hallows',
+        genre: "Children's Fiction",
+      },
+      {
+        title: "Harry Potter and the Philosopher's Stone",
+        genre: "Children's Fiction",
+      },
+    ],
+  });
+};
+
 export default function Index() {
+  const { books } = useLoaderData() as LoaderData;
   const [count, setCount] = useState(0);
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
@@ -19,29 +46,14 @@ export default function Index() {
 		  <button onClick={() => setCount(count + 1)}>Increment</button>
 		  <button onClick={() => setCount(count - 1)}>Decrement</button>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+      {books.map(({ title, genre }, i) => {
+          return (
+            <li key={i}>
+              <h3> {title} </h3>
+              <p> {genre} </p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
