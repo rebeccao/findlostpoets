c16cbf207bb9e463dec9012d535258a7203b1924
rebeccao <rebecca@stylsavvy.com>
2024-02-13 12:21:40 -0600

    Fixed the checkbox that disappeared after upgrading TailwindCSS.

--- app/components/sidebar/sidebar-panel.tsx
+++ app/components/sidebar/sidebar-panel.tsx
@@ -31,11 +31,14 @@ const SidebarPanel: React.FC<SidebarPanelProps> = ({ onSelectionChange }) => {
   };
 
   const handleCheckboxChange = (title: string, dbField: string) => {
-    if (title && dbField && selectedCheckbox !== title) {
+    if (title && selectedCheckbox !== title) {
       setSelectedCheckbox(title);
 
       // Complex DB query
       const dbQuery = {
+        where: {
+          [dbField]: { gt: 0 },
+        },
         orderBy: {
           [dbField]: "asc", // Use dynamic field name
         },
@@ -102,27 +105,27 @@ const SidebarPanel: React.FC<SidebarPanelProps> = ({ onSelectionChange }) => {
                 (expandedSidebarItem, expandedSidebarItemIndex) => (
                   <div
                     key={`${sidebarItem.title}-${expandedSidebarItemIndex}`}
-                    className="flex items-center p-2 pl-8 rounded hover:bg-gray-100 dark:hover:bg-gray-600"
+                    className="flex items-center p-2 pl-8 rounded hover:bg-gray-100 dark:hover:bg-gray-600 w-full"
                   >
                     {sidebarItem.type === "checkbox" ? (
-                      <label
-                        className="w-full flex items-center cursor-pointer"
-                        htmlFor={index.toString()}
-                      >
-                        <input
-                          id={index.toString()}
-                          type="checkbox"
-                          value={expandedSidebarItem.dbField}
-                          checked={
-                            selectedCheckbox === expandedSidebarItem.title
-                          }
-                          onChange={() =>
-                            handleCheckboxChange(expandedSidebarItem.title!, expandedSidebarItem.dbField) 
-                          }
-                          className="appearance-none w-4 h-4 border border-gray-200 rounded-sm shadow-sm bg-white mr-2"
-                        />
-                        {expandedSidebarItem.title}
-                      </label>
+                      <label className="w-full flex items-center cursor-pointer" htmlFor={index.toString()}>
+                      <input
+                        id={index.toString()}
+                        type="checkbox"
+                        value={expandedSidebarItem.dbField}
+                        checked={selectedCheckbox === expandedSidebarItem.title}
+                        onChange={() => handleCheckboxChange(expandedSidebarItem.title!, expandedSidebarItem.dbField)}
+                        className="opacity-0 absolute h-4 w-4"
+                      />
+                      <span className={`ml-1 inline-block w-4 h-4 rounded-sm shadow-sm flex justify-center items-center mr-2 ${selectedCheckbox === expandedSidebarItem.title ? 'bg-gray-400 border-gray-700' : 'bg-white border border-gray-200'}`}>
+                        {selectedCheckbox === expandedSidebarItem.title && (
+                          <svg className="w-3 h-3 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                           <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                          </svg>
+                        )}
+                      </span>
+                      {expandedSidebarItem.title}
+                    </label>
                     ) : sidebarItem.type === "search" ? (
                       <div className="relative rounded-md shadow-sm">
                         <div
