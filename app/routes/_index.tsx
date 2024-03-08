5358eb19bffe8d6ee014986cbba8bfba57f8eca2
rebeccao <rebecca@stylsavvy.com>
2024-03-08 14:47:13 -0600

    Fixes to prisma schema to allow more optional blank fields. Infinite scroll logic added (that is currently not working).

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import { useLoaderData, useFetcher } from '@remix-run/react'
 import { json } from '@remix-run/node'
 import type { LoaderFunction } from '@remix-run/node'
 import { prisma } from '~/utils/prisma.server'
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import SidebarPanel from '~/components/sidebar/sidebar-panel';
 import ImageCard from '~/components/image-card';
 import {HiMenuAlt3} from 'react-icons/hi';
@@ -79,55 +79,46 @@ function Sidebar({
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
-	console.log('Index loader: start');
+
 	try {
 		const url = new URL(request.url);
 
-		let dbQuery: SearchCriteria = { orderBy: [{ pid: 'asc' }] }; // query on first load
+		let dbQuery: SearchCriteria = { orderBy: [{ pid: 'asc' }], skip: 0, take: 20 }; // query on first load
 		
 		const searchQuery = url.searchParams.get("query");
 		console.log('Index loader: Received searchQuery:', searchQuery);
 
-		let skip = 0; // Default value
-		let take = 20; // Default value
-
 		if (searchQuery) {
-				try {
-						const dbSearchQuery = JSON.parse(decodeURIComponent(searchQuery));
-						console.log('Index loader: Parsed dbSearchQuery:', dbSearchQuery);
-
-						// Now extract skip and take directly from the parsed Prisma query, if they exist
-						if (dbSearchQuery.skip !== undefined) {
-								skip = dbSearchQuery.skip;
-						}
-
-						if (dbSearchQuery.take !== undefined) {
-								take = dbSearchQuery.take;
-						}
-
-						// Ensure the rest of the dbSearchQuery is applied to dbQuery excluding skip and take
-						const { skip: _, take: __, ...restQuery } = dbSearchQuery;
-						if (Object.keys(restQuery).length > 0) {
-								dbQuery = { ...dbQuery, ...restQuery };
-						}
-				} catch (error) {
-						console.error('Index loader: Error parsing criteriaParam:', error);
-						// Consider returning an error response or handling the error gracefully
-				}
+			try {
+				const parsedQuery: SearchCriteria = JSON.parse(decodeURIComponent(searchQuery));
+				// see if it works after removing the merge of the 2 queries. Have a conditional to do the query instead
+				dbQuery = { ...dbQuery, ...parsedQuery };
+			} catch (error) {
+				console.error('Error parsing search query:', error);
+			}
 		}		
 
-		console.log('Index skip = ', skip);
-
-		const poets = await prisma.poet.findMany({
-			...dbQuery,
-			skip, 
-			take,
-		});
+		const poets = await prisma.poet.findMany({ ...dbQuery });
 		return json({ poets });
-	} catch (error) {
-		console.error(error);
-		// Return an error response with a status code, e.g., 400 for a bad request or 500 for server error
-		return json({ error: "500 Internal Server Error. Failed to load poets due to a server error. Index route, loader: LoadFunction. Please contact Support." }, { status: 500 });
+
+	} catch (error: unknown) {
+		console.error('Loader error:', error);
+		let errorDetail;
+	  if (error instanceof Error) {
+			// Fallback for general errors
+			errorDetail = {
+					message: error.message,
+			};
+		} else {
+				// Handle unknown errors
+				errorDetail = {
+						message: 'An unknown error occurred',
+				};
+		}
+		return json({ 
+			error: "Server Error. Index route, loader: LoadFunction. Please contact Support.", 
+			detail: errorDetail
+		}, { status: 500 });
 	}
 };
 
@@ -139,6 +130,95 @@ function Index() {
   const { poets, error } = fetcher.data || initialData;
 	console.log("******************* const poets: Poet[] = data.poets, fetcher.data ", fetcher.data); // Check the structure of 'data'
 	console.log("******************* const poets: Poet[] = data.poets, initialData ", initialData); 
+
+  const sentinelRef = useRef<HTMLDivElement | null>(null);
+  const [currentDbQuery, setCurrentDbQuery] = useState<SearchCriteria>({ orderBy: [{ pid: 'asc' }], take: 20, skip: 0 });
+
+  interface PaginationState {
+    skip: number;
+    take: number;
+    hasMore: boolean;
+  }
+
+  const [pagination, setPagination] = useState<PaginationState>({
+    skip: 0,
+    take: 20, // Adjust according to your initial page size
+    hasMore: true,
+  });
+
+	/*************** Infinite scroll logic ****************/
+
+	const fetchMorePoets = useCallback(() => {
+		if (fetcher.state !== 'idle' || !pagination.hasMore) return;
+	
+		console.log("fetchMorePoets pagination.skip = ", pagination.skip);
+		const nextSkip = pagination.skip + pagination.take;
+		console.log("fetchMorePoets nextSkip = ", nextSkip);
+		
+		const updatedQuery = { ...currentDbQuery, skip: nextSkip };
+		const queryString = encodeURIComponent(JSON.stringify(updatedQuery));
+		fetcher.load(`?index&query=${queryString}`);
+	}, [fetcher, pagination, currentDbQuery]);
+
+	// Set up the IntersectionObserver to fetchMorePoets when the sentinel element
+	// becomes fully visible and there are additional poets are available to fetch
+  useEffect(() => {
+		console.log("IntersectionObserver --- useEffect");
+		console.log("IntersectionObserver --- pagination.hasMore = ", pagination.hasMore);
+		console.log("IntersectionObserver --- fetcher.state = ", fetcher.state);
+
+		// Only create the observer if we have more items to fetch
+		if (!pagination.hasMore) return;
+	
+		const observer = new IntersectionObserver((entries) => {
+			console.log("IntersectionObserver --- entries[0].isIntersecting = ", entries[0].isIntersecting);
+			// Check if the sentinel is visible
+			if (entries[0].isIntersecting && fetcher.state === 'idle') {
+				fetchMorePoets();
+			}
+		}, { 
+			threshold: 0.1, // Slightly above 0 to require a bit more of the element to be visible
+			rootMargin: '100px' // Trigger before the sentinel is actually reached
+		});
+	
+		const currentSentinel = sentinelRef.current;
+
+    // Attach the observer if the fetcher is idle
+    if (currentSentinel && fetcher.state === 'idle') {
+      observer.observe(currentSentinel);
+			console.log("IntersectionObserver --- observer.observe(currentSentinel)")
+    }
+
+    return () => {
+      // Detach the observer when the component unmounts or if fetcher starts loading
+      if (currentSentinel) {
+        observer.unobserve(currentSentinel);
+				console.log("IntersectionObserver --- observer.observe(currentSentinel)")
+      }
+    };
+  }, [pagination.hasMore, fetcher.state, fetchMorePoets]);
+
+	// handle the response from the fetch operation to update pagination.
+	useEffect(() => {
+		console.log("pagination --- useEffect");
+		//console.log("fetcher.data length = ", fetcher.data.length);
+		console.log("pagination --- fetcher.state = ", fetcher.state);
+		// Check if there's a recent fetch operation and it's completed with data
+		if (fetcher.data && fetcher.state === 'idle') {
+			// Assuming fetcher.data includes the fetched poets and possibly an indication of whether more poets are available
+			// const hasMore = fetcher.data.length === pagination.take; // Simple check; adjust based on actual logic needed
+			const hasMore = true; // Add hasMore logic once the inifinite scrolling is working  
+	
+			setPagination((prevPagination) => ({
+				...prevPagination,
+				skip: prevPagination.skip + prevPagination.take, // Update skip to the next set of items
+				hasMore, // Update based on actual condition to determine if more items are available
+			}));
+			console.log("pagination --- pagination.skip = ", pagination.skip);
+		}
+	}, [fetcher.data, fetcher.state]);
+
+	/*************** SidebarPanel callback logic ****************/
 
 	const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -155,15 +235,20 @@ function Index() {
 
 	// Callback from SidebarPanel when the user clicks the Search button
 	const performSearch = (query: SearchCriteria) => {
-		query.where !== undefined ? query.skip = 0 : query.skip = (Math.floor(Math.random() * 28149) + 1);
-		
-		const queryString = JSON.stringify(query);
-    console.log('Index performSearch: queryString));', queryString);
 
 		// Signal that a search has been initiated. This state conditionally controls displaying the Rare Trait count on the ImageCard 
 		setSearchButtonPressed(true);
-    
-		const dbQueryString = new URLSearchParams({ query: queryString }).toString();
+
+		query.where !== undefined ? query.skip = 0 : query.skip = (Math.floor(Math.random() * 28149) + 1);
+
+		setCurrentDbQuery(query);
+		console.log('Index performSearch: query));', query);
+		
+		//const queryString = JSON.stringify(query);
+  	//const dbQueryString = new URLSearchParams({ query: queryString }).toString();
+		
+		// Serialize and encode the query
+		const dbQueryString = encodeURIComponent(JSON.stringify(query));
 		
 		// Use fetcher.load to initiate the request
 		fetcher.load(`?index&${dbQueryString}`);              // Note: the following doesn't work: fetcher.load(`/?${queryString}`);
@@ -234,15 +319,16 @@ function Index() {
 						{error && <div className="error">Error: {error}</div>}
 
 						<div className="grid grid-cols-4 gap-4">
-						{poets?.map((poet: Poet) => (
+						{poets?.map((poet: Poet, index: number) => (
 							<ImageCard 
 								key={poet.pid} 
-								poet={poet} 
+								poet={poet}
+								ref={index === poets.length - 1 ? sentinelRef : undefined} // Assign ref to last poet
 								// Dynamically access the Poet property
 								rarityTraitLabel={searchButtonPressed ? `${poet[selectedRareTraitLabel as keyof Poet]}` : undefined}
 								// Dynamically access the rarity count
-    						rarityCount={searchButtonPressed && selectedRareTrait ? poet[selectedRareTrait as keyof Poet] as number : undefined}
-								/>
+								rarityCount={searchButtonPressed && selectedRareTrait ? poet[selectedRareTrait as keyof Poet] as number : undefined}
+							/>
 						))}
 						</div>
 					</div>
