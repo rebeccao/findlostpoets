2d0f2e003a1be443b2ae8375c983bc7085f8b775
rebeccao <rebecca@stylsavvy.com>
2024-04-26 13:24:53 -0600

    Coming Soon page tweak.

--- app/components/coming-soon.tsx
+++ app/components/coming-soon.tsx
@@ -5,7 +5,7 @@ const ComingSoon = () => {
         <div className="h-screen flex items-center justify-center bg-gray-100 text-gray-800">
             <div className="text-center">
                 <h1 className="text-4xl font-bold">FindLostPoets</h1>
-                <h2 className="text-lg mt-2">Is coming soon!</h2>
+                <h1 className="text-lg mt-2">is coming soon!</h1>
             </div>
         </div>
     );
