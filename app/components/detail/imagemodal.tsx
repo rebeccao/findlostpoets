07a983887c692aafd7ae1bb319e4b0ebe0edbea2
rebeccao <rebecca@stylsavvy.com>
2024-05-15 14:42:59 -0600

    Converted css styles defined in tailwind.css to inline tailwindcss.

--- app/components/detail/imagemodal.tsx
+++ app/components/detail/imagemodal.tsx
@@ -22,7 +22,7 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageSrc, imag
     if (isOpen) {
       setShouldRender(true);  // Render the modal
       setTimeout(() => setShowModal(true), 10); // Slightly delay setting showModal to true to ensure CSS transition occurs
-      setScale(1); //minScale);   // If it's a 2X image, start at half size (1024/2048)
+      setScale(1); 
     } else {
       setShowModal(false); // Start the opacity transition immediately
       setTimeout(() => setShouldRender(false), 1000); // Remove from DOM after the transition
@@ -40,14 +40,14 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageSrc, imag
   }, [onClose]);
 
   const handleZoomIn = () => setScale(scale => Math.min(scale * 1.25, 2));
-  const handleZoomOut = () => setScale(scale => Math.max(scale / 1.25, 1)); //minScale));  // Do not zoom out beyond 1024x1024 equivalent
+  const handleZoomOut = () => setScale(scale => Math.max(scale / 1.25, 1)); 
 
   if (!shouldRender) return null;
 
   return (
-    <div className={`image-container fixed inset-0 bg-closetoblack flex justify-center items-center z-50 overflow-auto transition-opacity ease-in-out duration-1000 ${showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <div style={{ transform: `scale(${scale})` }} className="img-content transition-transform duration-300 cursor-pointer">
-        <img src={imageSrc} alt="Enlarged view" /> {/*className="w-auto max-w-none h-auto max-h-none object-contain" />*/}
+    <div className={`fixed inset-0 bg-closetoblack flex justify-center items-center z-50 overflow-auto transition-opacity ease-in-out duration-1000 ${showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+      <div style={{ transform: `scale(${scale})` }} className="relative w-full max-w-[1024px] h-full max-h-[1024px] overflow-hidden transition-transform duration-300 cursor-pointer">
+        <img src={imageSrc} alt="Enlarged view" className="w-full h-auto object-contain" />
       </div>
       <div className="absolute top-5 left-5 flex space-x-2">
         <button onClick={onClose} className="text-pearlwhite text-3xl">
