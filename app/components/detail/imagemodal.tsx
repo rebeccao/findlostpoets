b4a88f6e55b91d4be4e94311419255c8ae78412a
rebeccao <rebecca@stylsavvy.com>
2024-05-15 14:33:54 -0600

    More fine-tuning to get the 2X image to display in correct 1024x1024 size and to zoom in/out.

--- app/components/detail/imagemodal.tsx
+++ app/components/detail/imagemodal.tsx
@@ -11,19 +11,18 @@ interface ImageModalProps {
 
 const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageSrc, imageSize }) => {
   // Constants for initial and minimum scales based on image size
-  const initialScale = 1;                           // Start at full size for 1X, half for 2X
   const minScale = imageSize === '2X' ? 0.5 : 1;    // Minimum scale for 2X is 0.5 (1024px view)
 
   const [showModal, setShowModal] = useState(false);
   const [shouldRender, setShouldRender] = useState(false);
-  const [scale, setScale] = useState(initialScale);
+  const [scale, setScale] = useState(1);
 
 
   useEffect(() => {
     if (isOpen) {
       setShouldRender(true);  // Render the modal
       setTimeout(() => setShowModal(true), 10); // Slightly delay setting showModal to true to ensure CSS transition occurs
-      setScale(initialScale);
+      setScale(1); //minScale);   // If it's a 2X image, start at half size (1024/2048)
     } else {
       setShowModal(false); // Start the opacity transition immediately
       setTimeout(() => setShouldRender(false), 1000); // Remove from DOM after the transition
@@ -40,15 +39,15 @@ const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, imageSrc, imag
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [onClose]);
 
-  const handleZoomIn = () => setScale(scale => scale * 1.25);                       // Increase scale
-  const handleZoomOut = () => setScale(scale => Math.max(scale / 1.25, minScale));  // Do not zoom out beyond 1024x1024 equivalent
+  const handleZoomIn = () => setScale(scale => Math.min(scale * 1.25, 2));
+  const handleZoomOut = () => setScale(scale => Math.max(scale / 1.25, 1)); //minScale));  // Do not zoom out beyond 1024x1024 equivalent
 
   if (!shouldRender) return null;
 
   return (
-    <div className={`image-container fixed inset-0 bg-closetoblack flex justify-center items-center z-50 transition-opacity ease-in-out duration-1000 ${showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
-      <div style={{ transform: `scale(${scale})` }} className="flex flex-col items-center w-full h-full overflow-auto">
-        <img src={imageSrc} alt="Enlarged view" className={`${imageSize === '2X' ? "zoomed-img" : "normal-img"} transition-transform duration-300 cursor-pointer`} />
+    <div className={`image-container fixed inset-0 bg-closetoblack flex justify-center items-center z-50 overflow-auto transition-opacity ease-in-out duration-1000 ${showModal ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
+      <div style={{ transform: `scale(${scale})` }} className="img-content transition-transform duration-300 cursor-pointer">
+        <img src={imageSrc} alt="Enlarged view" /> {/*className="w-auto max-w-none h-auto max-h-none object-contain" />*/}
       </div>
       <div className="absolute top-5 left-5 flex space-x-2">
         <button onClick={onClose} className="text-pearlwhite text-3xl">
