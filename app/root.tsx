5e292ad09892a549fd643dd978d19f0734ca77be
rebeccao <rebecca@stylsavvy.com>
2024-05-25 20:54:19 -0600

    Changes added to address SEO Checker issues.

--- app/root.tsx
+++ app/root.tsx
@@ -26,8 +26,7 @@ export const links: LinksFunction = () => [
 export const meta: MetaFunction = () => [
   { charset: "utf-8" },
   { name: "viewport", content: "width=device-width,initial-scale=1.0" },
-  { name: "title", content: "FINDLOSTPOETS - Discover and Explore Murat Pak's LOSTPOETS NFT Collection" },
-  { name: "description", content: "FINDLOSTPOETS is a unique platform to explore Murat Pak's LOSTPOETS NFT collection. Browse GEN0 poets, GEN1 poets and their poems. Search poets by traits, rarities and word counts." },
+  { name: "description", content: "FINDLOSTPOETS - Explore Murat Pak's LOSTPOETS NFT collection. Browse and search Poets, their Poems and their traits." },
   { name: "keywords", content: "lostpoets, Murat, Pak, NFT, poets, poems, poetry, find lost poets, NFT, NFTs" },
   { name: "author", content: "0xNosToca" }
 ];
@@ -40,11 +39,12 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <title>FINDLOSTPOETS - Discover and Explore Murat Pak's LOSTPOETS NFT Collection</title>
       </head>
       <body>
         {/* Conditionally render Coming Soon page or the main Outlet based on environment */}
         {isProduction ? <ComingSoon /> : <Outlet />}
-        <h1 className="screenreader-only">Find Lost Poets</h1>
+        <h1 className="screenreader-only">FINDLOSTPOETS: Browse Poets, Poems and Traits</h1>
 
         {/* Manages scroll position for client-side transitions */}
         {/* If you use a nonce-based content security policy for scripts, you must provide the `nonce` prop. Otherwise, omit the nonce prop as shown here. */}
