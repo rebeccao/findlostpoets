1bdc5b97d76007bef4a41cdc47ace93a1672bca3
rebeccao <rebecca@stylsavvy.com>
2024-09-12 21:09:51 -0600

    Modal layout mods.

--- app/components/modals/baseinfomodal.tsx
+++ app/components/modals/baseinfomodal.tsx
@@ -14,9 +14,9 @@ const BaseModal: React.FC<BaseModalProps> = ({ onClose, isOpen, title, children,
   return (
     <div className={`relative px-5 pb-10 rounded-xl shadow-lg ${customWidth || 'w-[70vw]'} 'max-w-5xl' h-3/4 ${noScroll ? '' : 'overflow-y-auto'} border bg-closetoblack border-deepgray text-lightmedgray transition-transform duration-500 ease-out transform ${
       isOpen ? 'translate-y-0 opacity-100' : '-translate-y-10 opacity-0'}`}>
-      <div className="sticky top-0 bg-inherit flex justify-between items-center z-10 py-4">
+      <div className="sticky top-0 bg-inherit flex justify-center items-center z-10 py-4">
         <h2 className="text-2xl font-light">{title}</h2>
-        <button onClick={onClose} aria-label="Close">
+        <button onClick={onClose} aria-label="Close" className="absolute right-0">
           <GrClose size={24} />
         </button>
       </div>
